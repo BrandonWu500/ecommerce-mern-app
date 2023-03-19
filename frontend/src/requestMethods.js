6f9c06814cc0faaeaddc0d91596e7408c0ffeab6
BrandonWu500 <brwu17@gmail.com>
2023-03-19 16:15:22 -0400

    fix api url

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://brandonwu500-mern-shop-api.onrender.com'
+    ? 'https://brandonwu500-mern-shop-api.onrender.com/api'
     : 'http://localhost:3500/api';
 
 export const publicRequest = axios.create({
